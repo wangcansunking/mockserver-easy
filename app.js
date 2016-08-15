149184b7846b4f0fc86c89e27e72e143df7003c8
Wang Can <wac@tradeshift.com>
2016-08-15 14:15:08 +0800

    add file

--- app.js
+++ app.js
@@ -3,10 +3,27 @@
  */
 var express = require('express');
 
+var zlib = require('zlib');
+var iconv = require('iconv-lite');
+var sts = require('string-to-stream');
+
+
+
 var app = express();
 
 app.get('/', function (req, res) {
     res.json(require('./test.json'))
+});
+
+app.get('/file', function (req, res) {
+
+    var tempString = iconv.encode(JSON.stringify(require('./test.json')), 'gbk');
+
+    var gzip = zlib.createGzip();
+
+    sts(tempString).pipe(gzip).pipe(res);
+
+    res.flush();
 });
 
 app.listen(3000, function () {
